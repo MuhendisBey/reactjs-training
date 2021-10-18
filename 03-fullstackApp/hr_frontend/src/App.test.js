f743aa32f6b6f84f3c9da41b557331cf5fbcf6a8
Onur GURSOY <onur.gursoy@siemens.com>
2021-10-18 12:43:49 +0300

    hr project

--- 03-fullstackApp/hr_frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import HRApp from './HRApp';
-
-test('renders learn react link', () => {
-  render(<HRApp />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
